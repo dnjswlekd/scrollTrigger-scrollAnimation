f4e2e407327bb1753ccc5fc840d13205bcb4613a
dnjswlekd <wj8014@gmail.com>
2025-03-05 15:54:01 +0900

    imgbox scrolltrigger

--- js/script.js
+++ js/script.js
@@ -18,4 +18,19 @@ $(function () {
       { scale: 0.4, opacity: 0.3, duration: 10, ease: 'none' },
       5
     );
+
+  // img-box
+  gsap.utils.toArray('.img-box').forEach(function (imgBox) {
+    // toArray() => 요소를 배열로 만드는 메소드
+    // forEach() => 배열을 반복문으로 사용하는 메소드
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: imgBox,
+        start: '50% 100%',
+        toggleClass: { targets: imgBox, className: 'active' },
+        scrub: 1,
+        markers: true,
+      },
+    });
+  });
 });
